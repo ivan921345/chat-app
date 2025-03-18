848b0c6b8004238e98418b20aee790abadda4c2d
ivan921345 <98031928+ivan921345@users.noreply.github.com>
2025-03-18 17:54:56 +0100

    checking

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/chat-app/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
