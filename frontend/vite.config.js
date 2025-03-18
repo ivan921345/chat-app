59085eafdf6fc079f88f39cb02a6612dec8cc0d3
ivan921345 <98031928+ivan921345@users.noreply.github.com>
2025-03-18 16:15:12 +0100

    checking if gh-pages works

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: path.resolve(__dirname, "dist"),
-  },
 });
